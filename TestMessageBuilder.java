263bb9bf08235b1e0d502d9148cc5592bd569037
nagothuvenkat <72744709+nagothuvenkat@users.noreply.github.com>
2020-10-16 18:40:21 +0530

    Add files via upload
    Added the files

---
+++ TestMessageBuilder.java
@@ -0,0 +1,17 @@
+package com.javarticles.webapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMessageBuilder {
+
+    @Test
+    public void testNameMkyong() {
+
+        MessageBuilder obj = new MessageBuilder();
+        assertEquals("Hello mkyong", obj.getMessage("mkyong"));
+
+    }
+
+}
